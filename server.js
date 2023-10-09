5b7871dc3fc9a8838b88745926b74055626ecef0
Rayner Chames <rychames@gmail.com>
2023-10-09 01:24:22 -0400

    Primeiro COMMIT

---
+++ server.js
@@ -0,0 +1,25 @@
+// Importamos o módulo Express para o nosso arquivo.
+const express = require('express');
+
+// Criamos uma instância do Express. Esta instância nos permite definir rotas, configurar middleware, entre outras coisas.
+const app = express();
+
+// Definimos a porta onde o nosso servidor irá rodar. Neste caso, escolhemos a porta 3000, mas você pode escolher outra se preferir.
+const PORT = 3000;
+
+// Aqui, definimos uma rota. Esta rota é a rota raiz ("/") do nosso servidor.
+// Quando alguém acessar "http://localhost:3000/" no navegador, esta função será executada.
+app.get('/', (req, res) => {
+  // A função recebe dois argumentos: req (request ou requisição) e res (response ou resposta).
+  // req contém informações sobre a requisição feita ao servidor.
+  // res é um objeto que nos permite enviar respostas de volta ao cliente.
+
+  // Neste caso, estamos enviando a string 'Olá, Mundo!' de volta ao cliente.
+  res.send('Olá, Mundo!');
+});
+
+// Finalmente, dizemos ao nosso servidor para começar a escutar na porta que definimos anteriormente.
+// A função de callback é executada uma vez que o servidor esteja rodando e escutando naquela porta.
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
